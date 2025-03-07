cb2f719ff8d99065da9689d2b484934acbc5f202
bram de leeuw <bramdeleeuw@gmail.com>
2025-03-07 11:50:52 +0100

    Appelboomers theme first commit

---
+++ web/themes/appelboom/js/appelboom.js
@@ -0,0 +1,17 @@
+/**
+ * @file
+ * Appelboom behaviors.
+ */
+(function (Drupal) {
+
+  'use strict';
+
+  Drupal.behaviors.appelboom = {
+    attach (context, settings) {
+
+      console.log('It works!');
+
+    }
+  };
+
+} (Drupal));
