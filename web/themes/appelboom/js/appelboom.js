b55b6ecee707bb08bb6229c5a90171f71f4e31c9
bram de leeuw <bramdeleeuw@gmail.com>
2025-04-02 16:18:36 +0200

    excel-effect

--- web/themes/appelboom/js/appelboom.js
+++ web/themes/appelboom/js/appelboom.js
@@ -9,13 +9,45 @@
   Drupal.behaviors.appelboom = {
     attach (context, settings) {
 
-      console.log('It works!');
       $(document).on('click', '#hamburger', function(e){
       	e.preventDefault();
       	$('body').toggleClass('show-nav');
-      })
+      });
+
+      $('.homepage-header-image .field--name-field-featured-image:not(.excelatored)').each(function(){
+      		excelator($(this));
+  		});
 
     }
   };
+
+  var excelator = function($el){
+  	$el.addClass('excelatored');
+
+  	// add rows with divs
+  	let width = $el.width();
+  	let height = $el.height();
+  	let cellWidth = 100;
+  	let cellHeight = 30;
+  	let cellColumnCount = Math.floor(width / cellWidth);
+  	let cellRowCount = Math.floor(height / cellHeight);
+  	let Cells = $('<span class="cells" />');
+
+  	for (let j = 0; j <= cellRowCount; j++){
+	  	for (let i = 0; i <= cellColumnCount; i++) {
+	  		Cells.append($('<span class="cell" />'));
+	  	}
+	}
+  	// CSS: div on hover: change border-color
+  	// CSS: idle animation??
+
+  	Cells.appendTo($el);
+  	$('body').on('mouseenter', '.cell', function(){
+  		$(this).addClass('hover');
+  	});
+  	$('body').on('mouseleave', '.cell', function(){
+  		$(this).removeClass('hover');
+  	});
+  }
 
 }) (jQuery, Drupal, drupalSettings);
